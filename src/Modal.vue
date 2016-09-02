9627859b5d2097f23a5f9974aa296bf416e612b7
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2016-09-02 10:11:03 +0200

    Implemented draggable modals

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,11 +1,16 @@
 <template>
     <div role="dialog" class="modal" :class="{
         modeless: !backdrop,
+        movable:  movable,
+        dragging: dragging,
         fade:     effect === 'fade',
         zoom:     effect === 'zoom',
         in:       show,
     }" :style="{
         display: display ? 'block' : 'none',
+        top: position.y + 'px',
+        left: position.x + 'px',
+        transform: translateStyle,
     }" @click="backdropClick">
         <div :class="{
             'modal-dialog': true,
@@ -14,7 +19,9 @@
         }" role="document" :style="{width: optionalWidth}">
             <div v-el:modal-content class="modal-content">
                 <slot name="modal-header">
-                    <div class="modal-header">
+                    <div class="modal-header"
+                         @mousedown="startDrag" @touchstart="startDrag"
+                         @mouseup="stopDrag" @touchend="stopDrag">
                         <button type="button" class="close" @click="cancel">
                             &times;
                         </button>
@@ -82,6 +89,10 @@
                 type: Boolean,
                 default: true,
             },
+            movable: {
+                type: Boolean,
+                default: true,
+            },
             large: {
                 type: Boolean,
                 default: false,
@@ -94,6 +105,19 @@
         data() {
             return {
                 display: true,
+                dragging: false,
+                start: {
+                    x: 0,
+                    y: 0,
+                },
+                position: {
+                    x: 0,
+                    y: 0,
+                },
+                translate: {
+                    x: 0,
+                    y: 0,
+                },
             };
         },
         compiled() {
@@ -102,6 +126,9 @@
             });
         },
         computed: {
+            draggable() {
+                return !this.backdrop && this.movable;
+            },
             optionalWidth() {
                 if (this.width === null) {
                     return null;
@@ -109,6 +136,9 @@
                     return `${this.width}px`;
                 }
                 return this.width;
+            },
+            translateStyle() {
+                return `translate(${this.translate.x}px, ${this.translate.y}px)`;
             },
         },
         methods: {
@@ -154,6 +184,43 @@
                     }, 300);
                 }
             },
+            startDrag(e) {
+                if (!this.draggable || (!e.changedTouches && e.button != 0)) {
+                    return;
+                }
+                e.stopPropagation();
+                e.preventDefault();
+
+                e = e.changedTouches ? e.changedTouches[0] : e;
+                this.dragging = true;
+                this.start.x = e.pageX - this.position.x;
+                this.start.y = e.pageY - this.position.y;
+
+                document.body.addEventListener('mousemove', this.onDrag);
+                document.body.addEventListener('touchmove', this.onDrag);
+                document.body.addEventListener('mouseup', this.stopDrag);
+                document.body.addEventListener('touchend', this.stopDrag);
+            },
+            onDrag(e) {
+                e.stopPropagation();
+                e.preventDefault();
+
+                e = e.changedTouches ? e.changedTouches[0] : e
+                this.translate.x = e.pageX - this.start.x - this.position.x;
+                this.translate.y = e.pageY - this.start.y - this.position.y;
+            },
+            stopDrag(e) {
+                this.dragging = false;
+                this.position.x = e.pageX - this.start.x;
+                this.position.y = e.pageY - this.start.y;
+                this.translate.x = 0;
+                this.translate.y = 0;
+
+                document.body.removeEventListener('mousemove', this.onDrag);
+                document.body.removeEventListener('touchmove', this.onDrag);
+                document.body.removeEventListener('mouseup', this.stopDrag);
+                document.body.removeEventListener('touchend', this.stopDrag);
+            },
         }
     }
 </script>
@@ -162,8 +229,20 @@
         transition: all 0.3s ease;
     }
 
-    .modal.modeless {
+    .modal.movable {
         transition: opacity 0.3s ease;
+    }
+
+    .modal.movable .modal-header {
+        cursor: -webkit-grab;
+        cursor: -moz-grab;
+        cursor: grab;
+    }
+
+    .modal.movable.dragging .modal-header {
+        cursor: -webkit-grabbing;
+        cursor: -moz-grabbing;
+        cursor: grabbing;
     }
 
     .modal.in {
@@ -172,6 +251,9 @@
 
     .modal.in.modeless {
         background: transparent;
+    }
+
+    .modal.in.movable {
         bottom: initial;
         left: initial;
         right: initial;
